b04a51f5cb7d7be50b3ab60ce8c527ff263ba1a6
decimatedTomato <87151993+decimatedTomato@users.noreply.github.com>
2022-02-16 11:53:50 +0100

    Added constructor to Node

--- Node.py
+++ Node.py
@@ -1,11 +1,17 @@
 class Node:
-    edges = []
-    name = ""
+    name: str = ""
+    edges: list[tuple[str, str]] = []
+
+    def __init__(self, name):
+        self.name = name
+
     def listEdges(self):
         for i in self.edges:
             print(self.edges[i])
 
+
 class Edge:
     edge = ()
+
     def __init__(self, a, b):
         self.edge(a, b)
