b6a783cb22b2f74e9a3cf1b962f1dab478783f27
decimatedTomato <87151993+decimatedTomato@users.noreply.github.com>
2022-02-16 11:37:29 +0100

    Added an empty parse_input function

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def parse_input(file_name):
+    # open file
+
+    # Create instances of customer
+
+    # close file
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    parse_input("")
