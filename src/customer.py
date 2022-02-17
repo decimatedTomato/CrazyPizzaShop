1d1a67de5f4cea59637e5864b92758bee7db8c9e
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2022-02-17 16:19:29 +0100

    feat: ✨ update customer.py
    - Expand parms' accepted data types
    - Improve conflict_exists method
    - New common_loves function

--- src/customer.py
+++ src/customer.py
@@ -1,10 +1,29 @@
+from collections.abc import Iterable
 from dataclasses import dataclass
 
 
 @dataclass(eq=True, frozen=True)
 class Customer:
-    loves: tuple[str, ...]
-    dislikes: tuple[str, ...]
+    loves: Iterable[str]
+    dislikes: Iterable[str]
 
     def conflict_exists(self, other) -> bool:
-        return any(dislike in other.loves for dislike in self.dislikes)
+        """
+        Returns True if there is a conflict between the two customers
+
+        To be precise, return True if one likes something the other dislikes
+        """
+
+        return not set(self.loves).isdisjoint(other.dislikes)
+
+
+def common_loves(customers: Iterable[Customer]) -> set[str]:
+    """
+    Returns set of ingredients that are in common between all customers
+    """
+
+    result: set[str] = set()
+    for customer in customers:
+        result.intersection_update(customer.loves)
+
+    return result
