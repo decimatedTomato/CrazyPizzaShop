31e26d15026fe12fce04745f1fd7cf89e3654845
Minh Tran <33189614+minhtrancccp@users.noreply.github.com>
2022-02-17 16:46:59 +0100

    refactor: ♻️ update maximum_independent_set func
    Remove the usage of convert_node_labels_to_integers function

--- src/nodes.py
+++ src/nodes.py
@@ -1,4 +1,5 @@
 from collections.abc import Iterable
+from itertools import combinations
 from pprint import pprint
 
 from networkx import Graph, convert_node_labels_to_integers
@@ -6,28 +7,27 @@ from networkx import Graph, convert_node_labels_to_integers
 from src.customer import Customer
 
 
-def maximum_independent_set(customers: Iterable[Customer]):
+def maximum_independent_set(customers: Iterable[Customer]) -> Graph:
     """
-    Returns the maximum independent set of customers in the given list of customers.
+    Returns the maximum independent set of customers in the given list of customers
+
+    Function should return the largest set of customers with no internal conflicts
     """
+
     new_graph: Graph = Graph()
     new_graph.add_nodes_from(customers)
-    pprint(new_graph.nodes)
 
-    new_graph = convert_node_labels_to_integers(new_graph)
-    pprint(new_graph.nodes)
+    return new_graph
 
 
 def main():
-    """
-    Main function.
-    """
     customers = [
         Customer(("lobster", "tomato"), ("cheese", "potato")),
         Customer(("cheese", "tomato"), ("shrimp",)),
         Customer(("shrimp", "potato"), ("egg",)),
     ]
-    maximum_independent_set(customers)
+
+
 
 
 if __name__ == "__main__":
